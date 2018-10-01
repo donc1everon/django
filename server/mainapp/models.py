51f60285584442a3dcea78ffe26e520747158eca
donc1everon <it.antonov@gmail.com>
2018-10-01 23:03:06 +0300

    end less_03

--- server/mainapp/models.py
+++ server/mainapp/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    title = models.CharField(
+        max_length=250,
+        unique=True
+    )
+
+    snippet = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    modified = models.DateTimeField(
+        auto_now=True
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+class Product(models.Model):
+
+    alias = models.CharField(
+        max_length=250,
+        unique=True
+    )
+
+    title = models.CharField(
+        max_length=250,
+        unique=True
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    image = models.ImageField(
+        blank=True,
+        null=True,
+    )
+
+    snippet = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    cost = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0
+    )
+
+    modified = models.DateTimeField(
+        auto_now=True
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.alias
--- server/mainapp/urls.py
+++ server/mainapp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('catalog/', views.catalog_list, name='catalog'),
-    path('brooch/', views.product_detail, name='brooch'),
+    path('<int:pk>/', views.product_detail, name='brooch'),
     path('contacts/', views.contacts, name='contacts'),
 ]
